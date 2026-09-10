cb5e1d2ba9991fb3b0e1c3e0b4ccfe441fe29bfe
Angel K <angelkp1102@gmail.com>
2026-09-10 21:27:19 +0530

    Time: 1 ms (98.36%), Space: 42.5 MB (69.2%) - LeetHub

--- 3945-digit-frequency-score/3945-digit-frequency-score.java
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -1,11 +1,10 @@
 class Solution {
     public int digitFrequencyScore(int n) {
-      int sum = 0;
-      while( n>0){
-          int digit = n %10;
-          sum += digit;
-          n /=10;
-      }
-      return sum;  
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum ;
     }
 }
